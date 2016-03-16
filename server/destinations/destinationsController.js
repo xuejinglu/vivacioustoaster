1b14bbb2b80e1b246608a741628c708909daab50
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-16 16:56:50 -0700

    Stub destination controller

---
+++ server/destinations/destinationsController.js
@@ -0,0 +1,26 @@
+const Destination = require('./destinations');
+const Trip = require('../trips/trips');
+
+module.exports = {
+  // Params: req.body is an array of destination objects.
+  // Returns: array of created destinations
+  create: (req, res) => {
+    res.end();
+  },
+
+  get: (req, res) => {
+
+  },
+
+  getAll: (req, res) => {
+    res.end();
+  },
+
+  update: (req, res) => {
+    res.end();
+  },
+
+  delete: (req, res) => {
+    res.end();
+  },
+};
