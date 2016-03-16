90ae47838c77315174bb147b219a37d9c7269f0e
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-15 21:16:09 -0700

    Set up presets and plugins for karma testing

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,32 @@ module.exports = (config) => {
     preprocessors: {
       'tests.webpack.js': ['webpack'],
     },
+    plugins: [
+      require('karma-webpack'),
+      require('karma-mocha'),
+      require('karma-chrome-launcher'),
+    ],
     reporters: ['dots'],
+    webpack: {
+      module: {
+        loaders: [
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+              presets: ['es2015', 'react', 'stage-0'],
+              plugins: [
+                'react-html-attrs',
+                'transform-class-properties',
+                'transform-decorators-legacy',
+              ],
+            },
+          },
+        ],
+      },
+      watch: true,
+    },
     webpackServer: {
       noInfo: true,
     },
