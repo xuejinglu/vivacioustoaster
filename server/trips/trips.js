d41c5bb7bcde3b8661de58498e9ff5e34581c35d
Leran <leranf#gmail.com>
2016-03-16 14:20:51 -0700

    creates join table between users and trips

--- server/trips/trips.js
+++ server/trips/trips.js
@@ -1,10 +1,20 @@
 const db = require('../config/db');
 const Sequelize = require('sequelize');
+const User = require('../users/users');
 
 const Trip = db.define('trips', {
   name: Sequelize.STRING,
 });
 
+const UserTrip = db.define('userTrips', {
+  viewed: Sequelize.INTEGER,
+});
+
+User.belongsToMany(Trip, { through: UserTrip });
+Trip.belongsToMany(User, { through: UserTrip });
+
 Trip.sync();
+UserTrip.sync();
+User.sync();
 
 module.exports = Trip;
