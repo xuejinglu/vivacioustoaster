49f89c06ca0a91b617ec2a2d17b7e3d6ceeda328
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-16 17:26:32 -0700

    Add error handling in createAll and getAll

--- server/destinations/destinationsController.js
+++ server/destinations/destinationsController.js
@@ -1,11 +1,12 @@
 const _ = require('lodash');
+const helpers = require('../config/helpers');
 const Destination = require('./destinations');
 const Trip = require('../trips/trips');
 
 module.exports = {
   // Params: req.body is an array of destination objects.
   // Returns: array of created destinations
-  createAll: (req, res) => {
+  createAll: (req, res, next) => {
     // extend destination objects with tripId from url param
     const tripIds = Array(req.body.length).fill({ tripId: req.params.tripId });
     const newDestinations = _.merge(req.body, tripIds);
@@ -13,25 +14,33 @@ module.exports = {
     Destination.bulkCreate(newDestinations)
     .then((destinations) => {
       res.json(destinations);
+    })
+    .catch((err) => {
+      helpers.errorLogger(err, req, res, next);
+      helpers.errorHandler(err, req, res, next);
     });
   },
 
-  get: (req, res) => {
+  get: (req, res, next) => {
     res.end();
   },
 
-  getAll: (req, res) => {
+  getAll: (req, res, next) => {
     Destination.findAll({ where: { tripId: req.params.tripId } })
     .then((destinations) => {
       res.json(destinations);
+    })
+    .catch((err) => {
+      helpers.errorLogger(err, req, res, next);
+      helpers.errorHandler(err, req, res, next);
     });
   },
 
-  update: (req, res) => {
+  update: (req, res, next) => {
     res.end();
   },
 
-  delete: (req, res) => {
+  delete: (req, res, next) => {
     res.end();
   },
 };
