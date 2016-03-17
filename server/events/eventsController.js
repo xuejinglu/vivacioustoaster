742cbb3b054a53ef782d43a13f17032b4913c632
Leran <leranf#gmail.com>
2016-03-17 15:07:16 -0700

    moves tag related functions to its own controller

--- server/events/eventsController.js
+++ server/events/eventsController.js
@@ -42,41 +42,8 @@ module.exports = {
   },
 
   delete: (req, res, next) => {
-    // TODO
-    res.end();
-  },
-
-  // Params: req.body.tags is an array of tag objects.
-  // Returns: array of created tags
-  createTags: (req, res, next) => {
-    // extend tag objects with eventId from url param
-    const eventIds = Array(req.body.tags.length).fill({ eventId: req.params.eventId });
-    const newTags = _.merge(req.body.tags, eventIds);
-
-    Tag.bulkCreate(newTags)
-      .then((tags) => {
-        res.json(tags);
-      })
-      .catch((err) => {
-        helpers.errorHandler(err, req, res, next);
-      });
-  },
-
-  getTags: (req, res, next) => {
-    Event.findOne({ where: { id: req.params.eventId } })
-      .then((event) => {
-        // after we've selected the correct event, get it's tags with
-        // the Sequelize method given to us by the hasMany relationship
-        res.json(event.getTags());
-      })
-      .catch((err) => {
-        helpers.errorHandler(err, req, res, next);
-      });
-  },
-
-  deleteTag: (req, res, next) => {
-    Tag.destroy({ where: { id: req.params.tagId } })
-      .then((tag) => {
+    Event.destroy({ where: { id: req.params.destId } })
+      .then(() => {
         res.end();
       })
       .catch((err) => {
