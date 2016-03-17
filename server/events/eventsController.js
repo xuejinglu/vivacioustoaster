02e0a7c0757a8914048f9ca3c08431cd4514f30b
Leran <leranf#gmail.com>
2016-03-17 14:11:15 -0700

    clarifies getTags and deleteTag functions

--- server/events/eventsController.js
+++ server/events/eventsController.js
@@ -1,25 +1,31 @@
+const _ = require('lodash');
+const helpers = require('../config/helpers');
 const Event = require('./events');
 const Tag = require('../tags/tags');
 
 module.exports = {
 
-  getTags: (req, res) => {
-    const eventId = req.params.eventId;
-    Event.findOne({ where: { eventId } })
+  getTags: (req, res, next) => {
+    Event.findOne({ where: { id: req.params.eventId } })
       .then((event) => {
         res.json(event.getTags());
+      })
+      .catch((err) => {
+        helpers.errorHandler(err, req, res, next);
       });
   },
 
-  createTags: (req, res) => {
+  createTags: (req, res, next) => {
 
   },
 
-  deleteTag: (req, res) => {
-    const tagId = req.params.tagId;
-    Tag.findOne({ where: { tagId } })
+  deleteTag: (req, res, next) => {
+    Tag.findOne({ where: { id: req.params.tagId } })
       .then((tag) => {
         tag.destroy();
+      })
+      .catch((err) => {
+        helpers.errorHandler(err, req, res, next);
       });
   },
 
