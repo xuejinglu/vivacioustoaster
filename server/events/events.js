c8e37fcaf51b564ad4acbf8a64dc6877f3c193fb
Leran <leranf#gmail.com>
2016-03-17 16:09:49 -0700

    adds dest dependency to events, and creates one to many relationship with dests

--- server/events/events.js
+++ server/events/events.js
@@ -1,10 +1,18 @@
 const db = require('../config/db');
 const Sequelize = require('sequelize');
+const Destination = require('../destinations/destinations');
 
 const Event = db.define('events', {
   // event: this will have many attrs
 });
 
+// creates destId column in tags table
+Event.belongsTo(Destination);
+
+// will add methods to Destination (ex. Destination.getEvents())
+Destination.hasMany(Event);
+
 Event.sync();
+Destination.sync();
 
 module.exports = Event;
