130077ebd2221de3a0a8dc77a281fc8408e35484
Leran <leranf#gmail.com>
2016-03-18 10:58:10 -0700

    removes test data, will all live in one file

--- server/__tests__/usersController-test.jsx
+++ server/__tests__/usersController-test.jsx
@@ -1,70 +1,34 @@
 const expect = require('chai').expect;
 const Promise = require('bluebird');
+const Sequelize = require('sequelize');
 const db = require('../config/db');
 const User = require('../users/users');
 const UserController = require('../users/usersController');
 
-// Test user data
-var testUsers = [
-  {
-    fbId: 'Orson Scott Card',
-    name: 1954,
-    picUrl: 'Richland, Washington',
-  },
-  {
-    fbId: 'Orson Scott Card',
-    name: 1954,
-    picUrl: 'Richland, Washington',
-  },
-  {
-    fbId: 'Orson Scott Card',
-    name: 1954,
-    picUrl: 'Richland, Washington',
-  },
-  {
-    fbId: 'Orson Scott Card',
-    name: 1954,
-    picUrl: 'Richland, Washington',
-  }
-];
 
 // The `clearDB` helper function, when invoked, will clear the database
 // by dropping the db - this is what the { force: true } is for
-var clearDB = function () {
+const clearDB = () => {
   // this drops the db each time
-  return User.sync({ force: true });
+  User.sync({ force: true });
 };
 
-describe('User Controller', function () {
+describe('User Controller', () => {
 
-  beforeEach(function () {
-    return clearDB()
-      .then(() => {
-        // Promise.all takes an array of promises and only
-        // completes when all promises have been fulfilled
-        return Promise.all(testUsers.map(user =>
-          UserController.createOne(user);
-        ));
-      })
-      .then(function(users) {
-        // There's actually no need for this particular then 
-        // block - it's just to show you how to turn the very 
-        // bulky sequelize model into usable information 
-        // Uncomment the console logs to see the difference!
-        var formattedUsers = users.map(user =>
-          user.toJSON();
-        });
-        // console.log(users);
-        // console.log(formattedUsers);
-      });
+  beforeEach(() => clearDB()
+    .then(() => 
+      // Promise.all takes an array of promises and only
+      // completes when all promises have been fulfilled
+      Promise.all(testUsers.map(user => UserController.createOne(user)));
+    );
   });
   
-  it('should have a method that retrieves all records from the database', function () {
+  it('should have a method that retrieves all records from the database', () => {
     // Your code here! 
     // Hint: Research how to write asynchronous tests using promises with mocha
   });
 
-  it('should have a method that creates a record in the database', function () {
+  it('should have a method that creates a record in the database', () => {
     // Your code here! 
     // Hint: Research how to write asynchronous tests using promises with mocha
   });
