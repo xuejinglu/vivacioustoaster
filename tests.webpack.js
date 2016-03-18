c9a8e6ec09aace52e8a9bfd65f3c301585947689
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-18 15:36:21 -0700

    Remove server tests from webpack bundle

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,4 +1,2 @@
 const clientContext = require.context('./client', true, /-test\.jsx?$/);
-const serverContext = require.context('./server', true, /-test\.jsx?$/);
 clientContext.keys().forEach(clientContext);
-serverContext.keys().forEach(serverContext);
