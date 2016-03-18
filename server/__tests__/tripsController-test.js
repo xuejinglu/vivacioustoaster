257cc2dbeae08507b8e0830856fcda01feef74ab
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-18 15:49:28 -0700

    Use .js not .jsx test files

--- server/__tests__/tripsController-test.js
+++ server/__tests__/tripsController-test.js
@@ -1,4 +1,7 @@
-import expect from 'expect';
+const expect = require('expect');
+const Promise = require('bluebird');
+const User = require('../users/users');
+const testData = require('./testData.js');
 
 describe('Trip Controller', () => {
   before(() => {
