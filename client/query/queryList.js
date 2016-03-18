babdaf8dc583b044779fd263b5a3e38de90aab38
Jing Lu <jing.xuejing.lu@gmail.com>
2016-03-17 17:03:53 -0700

    implement backbuttons

--- client/query/queryList.js
+++ client/query/queryList.js
@@ -2,12 +2,15 @@ import React from 'react';
 import QueryItem from './queryItem';
 import NavigationArrowForward
 from '../../node_modules/material-ui/lib/svg-icons/navigation/arrow-forward';
+import NavigationArrowBack
+from '../../node_modules/material-ui/lib/svg-icons/navigation/arrow-back';
 import { Link } from 'react-router';
 
 const QueryList = () => (
   <div>
   we are on QueryList page!
   <QueryItem />
+  <Link to="tag"><NavigationArrowBack /></Link>
   <Link to="tripPlan"><NavigationArrowForward /></Link>
   </div>
 );
--- client/tag/tagList.js
+++ client/tag/tagList.js
@@ -2,12 +2,15 @@ import React from 'react';
 import TagItem from './tagItem';
 import NavigationArrowForward
 from '../../node_modules/material-ui/lib/svg-icons/navigation/arrow-forward';
+import NavigationArrowBack
+from '../../node_modules/material-ui/lib/svg-icons/navigation/arrow-back';
 import { Link } from 'react-router';
 
 const TagList = () => (
   <div>
   we are on TagList page!
   <TagItem />
+  <Link to="friend"><NavigationArrowBack /></Link>
   <Link to="query"><NavigationArrowForward /></Link>
   </div>
 );
