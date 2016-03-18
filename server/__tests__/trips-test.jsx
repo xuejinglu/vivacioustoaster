ffed3cca323b1235fe3b387471903b8a64f21629
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-17 20:29:49 -0700

    Stub trips model and controller tests

---
+++ server/__tests__/trips-test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+
+describe('Trip Model', () => {
+  before(() => {
+    // connect to db, if necessary
+  })
+  beforeEach(() => {
+    // clear db
+    // create trips
+  });
+
+  it('creates a new trip', () => {
+    console.log('created trip');
+  });
+
+  it('gets a trip', () => {
+    // TODO
+  });
+
+  it('deletes a trip', () => {
+    // TODO
+  });
+});
---
+++ server/__tests__/tripsController-test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+
+describe('Trip Controller', () => {
+  before(() => {
+    // connect to db, if necessary
+  })
+  beforeEach(() => {
+    // clear db
+    // create trip
+  });
+
+  it('creates a new trip', () => {
+    console.log('created trip');
+  });
+
+  it('gets a trip', () => {
+    // TODO
+  });
+
+  it('deletes a trip', () => {
+    // TODO
+  });
+});
