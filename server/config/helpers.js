a9a55d64894af6ee3889a616f0fc3576d2d8aec1
Boya Jiang <jiang.boya@yahoo.com>
2016-03-19 15:44:58 -0700

    add decode token function to backEnd helpers

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -1,8 +1,28 @@
+const jwt = require('jwt-simple');
+
 module.exports = {
   errorHandler: (err, req, res, next) => {
     console.error(err.stack);
     // send error message to client
     // message for graceful error handling on app
     res.status(500).send({ error: err.message });
+  },
+
+  decode: (req, res, next) => {
+    const token = req.headers['x-access-token'];
+
+    if (!token) {
+      return res.send(403); // send forbidden if a token is not provided
+    }
+
+    try {
+      // decode token and attach user to the request
+      // for use inside our controllers
+      const user = jwt.decode(token, 'secret');
+      req.user = user;
+      next();
+    } catch (error) {
+      return next(error);
+    }
   },
 };
