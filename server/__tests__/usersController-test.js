0e2ff73ac306d7051ca26da02ee839b14ef40fa8
Leran <leranf#gmail.com>
2016-03-18 19:36:57 -0700

    comments untested functions

--- server/__tests__/usersController-test.js
+++ server/__tests__/usersController-test.js
@@ -2,10 +2,10 @@ const expect = require('expect');
 const Promise = require('bluebird');
 const rp = require('request-promise');
 const User = require('../users/users');
-const Trip = require('../trips/trips');
-const Destination = require('../destinations/destinations');
-const Event = require('../events/events');
-const Tag = require('../tags/tags');
+// const Trip = require('../trips/trips');
+// const Destination = require('../destinations/destinations');
+// const Event = require('../events/events');
+// const Tag = require('../tags/tags');
 const UserController = require('../users/usersController');
 const testData = require('./testData');
 
@@ -13,36 +13,36 @@ const testData = require('./testData');
 // by dropping the db - this is what the { force: true } is for
 const clearDB = () => Promise.all([
   User.sync({ force: true }),
-  Trip.sync({ force: true }),
-  Destination.sync({ force: true }),
-  Event.sync({ force: true }),
-  Tag.sync({ force: true }),
+  // Trip.sync({ force: true }),
+  // Destination.sync({ force: true }),
+  // Event.sync({ force: true }),
+  // Tag.sync({ force: true }),
 ]);
 
 const testUsers = testData.testUsers;
 describe('User Controller', () => {
 
-  beforeEach(() => clearDB().then(() => {
-    return Promise.all(testUsers.map(user => UserController.createOne(user)));
+  beforeEach(() => clearDB().then(() =>
+    Promise.all(testUsers.map(user => UserController.createOne(user)));
     // User.findOne({ where : {name: 'Leran Firer' }})
     //   .then(user => {
     //     User.findOne({ where : {name: 'Akshay Buddiga' }})
     //       .then(friend => user.addFriends(friend));
     //   });
-  }));
+  ));
 
   xit('should retrieve a user from the database', () => {
-    const options = {
-      method: 'GET',
-      uri: 'api/me',
-      query: {
-        fbId: 'qw0-lm1-pt8',
-      },
-    };
+    // const options = {
+    //   method: 'GET',
+    //   uri: 'api/me',
+    //   query: {
+    //     fbId: 'qw0-lm1-pt8',
+    //   },
+    // };
 
-    rp(options).then(user => {
-      expect(user.name).to.equal('Jing Lu');
-    });
+    // rp(options).then(user => {
+    //   expect(user.name).to.equal('Jing Lu');
+    // });
   });
 
   xit('should retrieve a user\s friends from the database', () => {
