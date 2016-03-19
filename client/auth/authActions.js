16c409de6c2d9f8f5cf53bf151a5e77649a581fa
Boya Jiang <jiang.boya@yahoo.com>
2016-03-18 20:41:25 -0700

    resolve conflict

--- client/auth/authActions.js
+++ client/auth/authActions.js
@@ -1,11 +1,11 @@
-// import fetch from 'isomorphic-fetch';
+import fetch from 'isomorphic-fetch';
+import { routeActions } from 'react-router-redux';
+>>>>>>> draft client side functions for login, logout, and fbLogin
 
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
-
-// place your api calls HURRS
 
 export const requestLogin = () => ({
   type: LOGIN_REQUEST,
@@ -33,3 +33,47 @@ export const loginError = (message) => ({
 export const requestLogout = () => ({
   type: LOGOUT_REQUEST,
 });
+// place your api calls HURRS
+export const logout = () =>
+  dispatch => {
+    dispatch(requestLogout());
+    window.sessionStorage.removeItem('com.tripsApp');
+    dispatch(routeActions.push('/home'));
+  };
+
+export const login = () =>
+  dispatch => {
+    dispatch(requestLogin());
+
+    const userRequest = new Request('/api/users', {
+      method: 'get',
+      headers: {
+        user: window.sessionStorage,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    fetch(userRequest)
+      .then(response => {
+        console.log(response);
+        dispatch(routeActions.push('/home'));
+      })
+      .catch(err => {
+        console.log('Error on login:', err);
+      });
+  };
+
+export const fbLogin = () =>
+  dispatch => {
+    dispatch(requestLogin());
+    fetch('/login/facebook')
+        .then(response => {
+          console.log(response);
+          // set response.user to the state
+          window.sessionStorage.setItem('com.tripsApp', response.token);
+          dispatch(routeActions.push('/home'));
+        })
+        .catch(err => {
+          console.log('Error in facebook authentication:', err);
+        });
+  };
