f1c16583bbedbe08b4d8af7270549688649e1fc2
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-18 20:14:18 -0700

    Add PORT to .env variables

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 // dotenv sets up the process.env variables. Put env variables in a .env file in your root
 require('dotenv').config({ path: `${__dirname}/../.env` });
-const PORT = process.env.PORT || 1337;
+const PORT = process.env.PORT;
 const passport = require('passport');
 const Strategy = require('passport-facebook').Strategy;
 const db = require('./config/db.js');
