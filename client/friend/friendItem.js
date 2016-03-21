1cf41965fb0863dfe673fe485c783632db2d7c7e
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-21 16:52:35 -0700

    Add onClick functionality

--- client/friend/friendItem.js
+++ client/friend/friendItem.js
@@ -3,11 +3,11 @@ import Avatar from 'material-ui/lib/avatar';
 import ListItem from 'material-ui/lib/lists/list-item';
 
 const FriendItem = ({ key, name, picUrl, addedToTrip, onClick }) => (
-  <ListItem leftAvatar={<Avatar src={picUrl} />}>{name}</ListItem>
+  <ListItem leftAvatar={<Avatar src={picUrl} />} onClick={onClick} >{name}</ListItem>
 );
 
 FriendItem.propTypes = {
-  key: React.PropTypes.number.isRequired,
+  key: React.PropTypes.number,
   name: React.PropTypes.string.isRequired,
   picUrl: React.PropTypes.string.isRequired,
   addedToTrip: React.PropTypes.bool.isRequired,
