15c0e819c32d656abc4a7dfcba3ab3051bf96499
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-21 16:54:07 -0700

    Map friends to props

--- client/friend/friendList.js
+++ client/friend/friendList.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Immutable from 'immutable';
 import FriendItem from './friendItem';
 import { toggleAddFriend } from './friendActions';
 import NavigationArrowForward
 from '../../node_modules/material-ui/lib/svg-icons/navigation/arrow-forward';
 import { Link } from 'react-router';
 
-const mapStateToProps = state => ({
-  friends: state.get('friends'),
-});
+const mapStateToProps = state => ({ friends: state.friend.get('friends') });
 
 const mapDispatchToProps = dispatch => ({
   onFriendClick: id => dispatch(toggleAddFriend(id)),
@@ -17,15 +16,17 @@ const mapDispatchToProps = dispatch => ({
 let FriendList = ({ friends, onFriendClick }) => (
   <div>
     we are on FriendList page!
-    {friends.map(friend =>
-      <FriendItem key={ friend.id } {...friend} onClick={() => onFriendClick(friend.id)} />
-    )}
+    <ul>
+      {friends.map(friend =>
+        <FriendItem key={ friend.id } {...friend} onClick={() => onFriendClick(friend.id)} />
+      )}
+    </ul>
     <Link to="tag"><NavigationArrowForward /></Link>
   </div>
 );
 
 FriendList.propTypes = {
-  friends: React.PropTypes.array.isRequired,
+  friends: React.PropTypes.object.isRequired,
   onFriendClick: React.PropTypes.func.isRequired,
 };
 
