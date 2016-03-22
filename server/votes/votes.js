ede7bc4e9d8ec9614dc826471f6d872e1e40bec8
Leran <leranf#gmail.com>
2016-03-22 15:26:32 -0700

    removes constraints from votes

--- server/votes/votes.js
+++ server/votes/votes.js
@@ -7,13 +7,13 @@ const Vote = db.define('votes', {});
 
 // creates userId column in votes table
 // creates eventId column in votes table
-Vote.belongsTo(User);
-Vote.belongsTo(Event);
+Vote.belongsTo(User, { constraints: false });
+Vote.belongsTo(Event, { constraints: false });
 
 // will add methods to User (ex. User.getEvents())
 // will add methods to Event (ex. Event.getVotes())
-User.hasMany(Vote);
-Event.hasMany(Vote);
+User.hasMany(Vote, { constraints: false });
+Event.hasMany(Vote, { constraints: false });
 
 Vote.sync();
 User.sync();
