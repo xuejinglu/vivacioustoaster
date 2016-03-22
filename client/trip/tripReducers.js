8d1e7c3267c75b94952a28adfbd2d45c2ab2d565
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-22 14:45:14 -0700

    Implement select trip action

--- client/trip/tripReducers.js
+++ client/trip/tripReducers.js
@@ -3,10 +3,13 @@ import { SELECT_TRIP } from './tripActions';
 
 const initialState = Immutable.Map({
   trips: Immutable.List(),
+  selectedTrip: null,
 });
 
 const selectedTrip = (state = initialState, action) => {
   switch (action.type) {
+    case SELECT_TRIP:
+      return state.set('selectedTrip', action.payload.trip);
     default:
       return state;
   }
