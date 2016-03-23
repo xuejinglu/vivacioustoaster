1317b953d86ce33d0b201d004d6cecfd05b07d26
Leran Firer <leranf@gmail.com>
2016-03-22 19:18:13 -0700

    remove #4,8

--- client/app/header.js
+++ client/app/header.js
@@ -1,11 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { AppBar, Tabs, Tab } from 'material-ui';
 import { Link } from 'react-router';
-import { fbLogin } from '../auth/authActions';
 
 const Header = () => (
   <AppBar title="Name This">
-    <button onClick={fbLogin}> Facebook button </button>
     <Link to="/trip"><Tab label="My Trips" /></Link>
     <Link to="/home"><Tab label="Home" /></Link>
   </AppBar>
