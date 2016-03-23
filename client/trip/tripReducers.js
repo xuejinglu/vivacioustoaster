d1731188aa7a46d69b67e7d951c60f68084fb696
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-22 17:39:28 -0700

    Remove select trip from tripReducers

--- client/trip/tripReducers.js
+++ client/trip/tripReducers.js
@@ -1,16 +1,14 @@
 import Immutable from 'immutable';
-import { SELECT_TRIP } from './tripActions';
 
 const initialState = Immutable.Map({
   trips: Immutable.List(),
-  selectedTrip: null,
 });
 
-const selectedTrip = (state = initialState, action) => {
+const trips = (state = initialState, action) => {
   switch (action.type) {
-    case SELECT_TRIP:
-      return state.set('selectedTrip', action.payload.trip);
     default:
       return state;
   }
 };
+
+export default trips;
