fd4e54f4e2517d4acf09a49112bc275e145b9a0f
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-22 17:40:05 -0700

    Implement fetchDestinations

--- client/tripPlan/dest/destActions.js
+++ client/tripPlan/dest/destActions.js
@@ -5,23 +5,41 @@ export const REQUEST_DESTINATIONS = 'REQUEST_DESTINATIONS';
 export const RECEIVE_DESTINATIONS = 'RECEIVE_DESTINATIONS';
 export const FETCH_DEST_FAILURE = 'FETCH_DEST_FAILURE';
 
-export const requestDestinations = (trip) => ({
+const requestDestinations = (trip) => ({
   type: REQUEST_DESTINATIONS,
   payload: {
     trip,
   },
 });
 
-export const receiveDestinations = (trip, json) => ({
+const receiveDestinations = (trip, destinations) => ({
   type: RECEIVE_DESTINATIONS,
   payload: {
     trip,
-    destinations: json.data.children.map(child => child.data),
+    destinations,
   },
 });
 
-export const fetchDestinationsError = (message) => ({
+const fetchDestinationsError = (message) => ({
   type: FETCH_DEST_FAILURE,
   payload: {
   },
 });
+
+export const fetchDestinations = trip =>
+  dispatch => {
+    // update state
+    dispatch(requestDestinations(trip));
+
+    return fetch(`/api/trips/${trip.id}/destinations`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then(res => res.json())
+      .then(destinations => {
+        dispatch(receiveDestinations(trip, destinations));
+      })
+      .catch(err => console.error(err)); // add proper error handling
+  };
