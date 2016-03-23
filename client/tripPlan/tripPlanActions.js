7a1912603e4c5dc70046ac20fe03c1fab3665af5
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-22 19:56:07 -0700

    Add helpful comments

--- client/tripPlan/tripPlanActions.js
+++ client/tripPlan/tripPlanActions.js
@@ -11,6 +11,11 @@ const selectTrip = (trip) => ({
   },
 });
 
+// Redux-Thunk Middleware (see configureStore.js) allows us to return a function that
+// can dispatch other actions. In this case, we return a function that:
+// (1) dispatches an action that updates the selectedTrip variable
+// (2) dispatches another action to GET destinations for the selected trip
+
 export const setTripAndGetDestinations = (trip) =>
   dispatch => {
     dispatch(selectTrip(trip));
