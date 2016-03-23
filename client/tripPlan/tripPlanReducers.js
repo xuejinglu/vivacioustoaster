1496fa61ae90dc32c16e3e5b1eef6ec7898d056d
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-22 19:29:27 -0700

    Remove immutable import

--- client/tripPlan/tripPlanReducers.js
+++ client/tripPlan/tripPlanReducers.js
@@ -1,11 +1,8 @@
 import { combineReducers } from 'redux';
-import Immutable from 'immutable';
 import { SELECT_TRIP } from './tripPlanActions';
 import dest from './dest/destReducers';
 import event from './event/eventReducers';
 import tripOverview from './tripOverview/travelFriendReducers';
-
-// const initialState = Immutable.Map();
 
 const selectedTrip = (state = {}, action) => {
   switch (action.type) {
