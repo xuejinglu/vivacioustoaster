1cbaa4daca7513237f7b11f8b22e162c69683033
Jing Lu <jing.xuejing.lu@gmail.com>
2016-03-23 10:36:15 -0700

    populate querylist page with events

--- client/query/queryItem.js
+++ client/query/queryItem.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Avatar from 'material-ui/lib/avatar';
+import ListItem from 'material-ui/lib/lists/list-item';
 
-const QueryItem = () => (
-  <div>
-  we are on QueryItem page!
-  </div>
+const QueryItem = ({ key, icon, name, address, rating }) => (
+  <ListItem
+    primaryText={name + ' rating:' + rating} // eslint-disable-line
+    secondaryText={address}
+    leftAvatar={<Avatar src={icon} />}
+  />
 );
+
+
+QueryItem.propTypes = {
+  key: React.PropTypes.string,
+  icon: React.PropTypes.string,
+  name: React.PropTypes.string,
+  address: React.PropTypes.string,
+  rating: React.PropTypes.number,
+};
 
 export default QueryItem;
