066f463e38bb20636e59c2099139e8ee6c352c36
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-22 17:41:40 -0700

    Add selectTrip to tripPlanReducer

--- client/tripPlan/tripPlanReducers.js
+++ client/tripPlan/tripPlanReducers.js
@@ -1,9 +1,23 @@
 import { combineReducers } from 'redux';
+import Immutable from 'immutable';
+import { SELECT_TRIP } from './tripPlanActions';
 import dest from './dest/destReducers';
 import event from './event/eventReducers';
 import tripOverview from './tripOverview/travelFriendReducers';
 
+// const initialState = Immutable.Map();
+
+const selectedTrip = (state = {}, action) => {
+  switch (action.type) {
+    case SELECT_TRIP:
+      return action.payload.trip;
+    default:
+      return state;
+  }
+};
+
 const tripPlanReducer = combineReducers({
+  selectedTrip,
   dest,
   event,
   tripOverview,
