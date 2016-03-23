36abc0b1a6c6e3b044a5d42345c76d6ed0a836a7
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-22 17:37:45 -0700

    Move selectedTrip state to tripPlan

--- client/query/queryActions.js
+++ client/query/queryActions.js
@@ -1,16 +1,19 @@
 import fetch from 'isomorphic-fetch';
+import { setTripAndGetDestinations } from '../tripPlan/tripPlanActions';
 
-export const save = (destinations, tripType, friends) => {
-  fetch('/api/trips', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      destinations,
-      tripType,
-      friends,
-    }),
-  });
-};
+export const save = (destinations, tripType, friends) =>
+  dispatch =>
+    fetch('/api/trips', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        destinations,
+        tripType,
+        friends,
+      }),
+    }).then(res => res.json())
+      .then(json => dispatch(setTripAndGetDestinations(json)))
+      .catch(err => console.error(err)); // add proper error handling
