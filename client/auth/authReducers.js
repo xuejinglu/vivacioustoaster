30a30508a0757d83067a4fb17378eb35d7f75396
Boya Jiang <jiang.boya@yahoo.com>
2016-03-24 12:27:06 -0700

    change default of user state to conform with auth wrapper

--- client/auth/authReducers.js
+++ client/auth/authReducers.js
@@ -2,7 +2,7 @@ import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT_REQUEST } from './a
 import { Map } from 'immutable';
 
 const initialState = Map({
-  user: null,
+  user: {},
   isAuthenticated: false,
   isFetchingAuth: false,
 }); // map auth state to the props
