6cf08ef42d99f763984728afa6cb2a8d3765ed5d
Leran <leranf#gmail.com>
2016-03-24 12:04:02 -0700

    renders trip name, type, and users

--- client/trip/tripItem.js
+++ client/trip/tripItem.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ListItem from 'material-ui/lib/lists/list-item';
+import List from 'material-ui/lib/lists/list';
 
 const TripItem = ({ key, name, tripType, destinations, users }) => (
-  <ListItem
-    primaryText={name}
-  />
+  <div>
+    <ListItem
+      primaryText={ name }
+      secondaryText={ tripType }
+    />
+    <List>
+      {users.map(user =>
+        <ListItem primaryText={ user.name } />
+      )}
+    </List>
+    <List>
+      {destinations.map(destination =>
+        <ListItem primaryText={ destination.location } />
+      )}
+    </List>
+  </div>
 );
 
 TripItem.propTypes = {
   key: React.PropTypes.number,
   name: React.PropTypes.string.isRequired,
   tripType: React.PropTypes.string.isRequired,
-  destinations: React.PropTypes.object.isRequired,
-  users: React.PropTypes.object.isRequired,
+  destinations: React.PropTypes.array.isRequired,
+  users: React.PropTypes.array.isRequired,
 };
 
 export default TripItem;
