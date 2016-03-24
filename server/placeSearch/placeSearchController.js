ddcf199fd30d0c1a241af59623f47dd3ac07cfe0
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-23 17:43:42 -0700

    Get places for multiple tags We now use `Promise.all` to make multiple Places API request, varying the types on each request. Once we have all the responses, we format and combine the places into one array.

--- server/placeSearch/placeSearchController.js
+++ server/placeSearch/placeSearchController.js
@@ -1,4 +1,6 @@
 const rp = require('request-promise');
+const Promise = require('bluebird');
+const _ = require('lodash');
 const helpers = require('../config/helpers');
 const tagClassifier = require('./tagClassifier');
 
@@ -10,29 +12,76 @@ module.exports = {
     const tags = req.body.tags;
 
     // Places API doesn't take more than 3 types, so we slice to get the first three
-    // types associated with the tag. I left the full type list intact so we can discuss
+    // types associated with each tag. I left the full type list intact so we can discuss
     // and experiment with which tags we should choose for the top 3.
-    const types = tagClassifier[tags[0]].slice(0, 3).join('|');
+    const tagTypes = tags.map(tag => ({
+      tag,
+      types: tagClassifier[tag].slice(0, 3).join('|'),
+    }));
 
-    const options = {
+    const optionsArray = tagTypes.map(tagType => ({
       uri: GOOGLE_PLACES_URL,
       qs: {
         key: process.env.GOOGLE_PLACES_KEY, // -> uri + '?key={{KEY}}'
         query: destinations[0].location,
-        types,
+        types: tagType.types,
       },
       headers: {
         'User-Agent': 'Request-Promise',
       },
       json: true, // Automatically parses the JSON string in the response
-    };
+      tripsAppTag: tagType.tag, // A hack so we have tag when constructing formatted place object
+    }));
 
-    rp(options)
-      .then(data => {
-        res.json(data);
-      })
-      .catch(err => {
-        res.status(500).end();
-      });
+    Promise.all(optionsArray.map(options =>
+      rp(options)
+        .then(data => {
+          const places = data.results;
+          return places.map(place => ({
+            address: place.formatted_address,
+            lat: place.geometry.location.lat,
+            lng: place.geometry.location.lng,
+            icon: place.icon,
+            name: place.name,
+            photo: place.photos ? place.photos[0] : null, // Object or null
+            placeId: place.place_id,
+            rating: place.rating,
+            tag: options.tripsAppTag,
+            addedToDest: false,
+          }));
+        })
+        .then(places => {
+          console.log('FORMATTED PLACESSSSSSS: ', places);
+          return places;
+        })
+        .catch(err => console.error(err))
+    ))
+    .then(placesArray => {
+      const allPlaces = _.flatten(placesArray);
+      console.log('final places array length is ', allPlaces.length);
+      res.json(allPlaces);
+    })
+    .catch(err => res.status(500).end());
+
+    // const options = {
+    //   uri: GOOGLE_PLACES_URL,
+    //   qs: {
+    //     key: process.env.GOOGLE_PLACES_KEY, // -> uri + '?key={{KEY}}'
+    //     query: destinations[0].location,
+    //     types: 'amusement_park|zoo|aquarium',
+    //   },
+    //   headers: {
+    //     'User-Agent': 'Request-Promise',
+    //   },
+    //   json: true, // Automatically parses the JSON string in the response
+    // };
+
+    // rp(options)
+    //   .then(data => {
+    //     res.json(data);
+    //   })
+    //   .catch(err => {
+    //     res.status(500).end();
+    //   });
   },
 };
