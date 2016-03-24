c6ca287ed2f30d756348f9c9754b7e9659367746
Leran <leranf#gmail.com>
2016-03-23 18:05:23 -0700

    renders events under destination

--- client/tripPlan/event/eventItem.js
+++ client/tripPlan/event/eventItem.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import ListItem from 'material-ui/lib/lists/list-item';
+import Card from 'material-ui/lib/card/card';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardMedia from 'material-ui/lib/card/card-media';
+import CardTitle from 'material-ui/lib/card/card-title';
+import CardText from 'material-ui/lib/card/card-text';
 
-const EventItem = () => (
-  <div>
-  we are on eventItem page!
-  </div>
+const EventItem = ({ key, name, formattedAddress, rating }) => (
+  <Card style={ { width: '50%', margin: '5%' } }>
+    <CardMedia
+      overlay={<CardTitle title={name} subtitle={`${rating} : ${formattedAddress}`} />}
+    >
+      <img src="http://lorempixel.com/500/300/nature/" />
+    </CardMedia>
+    <CardText>
+      Fill
+    </CardText>
+  </Card>
 );
+
+EventItem.propTypes = {
+  key: React.PropTypes.number,
+  name: React.PropTypes.string.isRequired,
+  formattedAddress: React.PropTypes.string.isRequired,
+  rating: React.PropTypes.number.isRequired,
+};
 
 export default EventItem;
--- client/tripPlan/event/eventList.js
+++ client/tripPlan/event/eventList.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import EventItem from './eventItem';
+import List from 'material-ui/lib/lists/list';
 
-const EventList = () => (
+const mapStateToProps = state => ({
+  events: state.tripPlan.event.get('events'),
+});
+
+let EventList = ({ events }) => (
   <div>
-  we are on EventList page!
-  <EventItem />
+    <List>
+      {events.map(event =>
+        <EventItem key={ event.id } {...event} />
+      )}
+    </List>
   </div>
 );
+
+EventList.propTypes = {
+  events: React.PropTypes.object.isRequired,
+};
+
+EventList = connect(mapStateToProps)(EventList);
+
 
 export default EventList;
