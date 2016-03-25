f219a4248ea4e8820893711545906cc5fc59366d
Leran <leranf#gmail.com>
2016-03-24 17:27:10 -0700

    creates a trip item displaying users and destinations

--- client/trip/tripItem.js
+++ client/trip/tripItem.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import ListItem from 'material-ui/lib/lists/list-item';
+import Card from 'material-ui/lib/card/card';
+import CardActions from 'material-ui/lib/card/card-actions';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardMedia from 'material-ui/lib/card/card-media';
+import CardTitle from 'material-ui/lib/card/card-title';
+import FlatButton from 'material-ui/lib/flat-button';
+import CardText from 'material-ui/lib/card/card-text';
 import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import GridList from 'material-ui/lib/grid-list/grid-list';
+import GridTile from 'material-ui/lib/grid-list/grid-tile';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500,
+    height: 1000,
+    overflowY: 'auto',
+    marginBottom: 24,
+  },
+};
 
 const TripItem = ({ key, name, tripType, destinations, users }) => (
-  <div>
-    <ListItem
-      primaryText={ name }
-      secondaryText={ tripType }
-    />
-    <List>
+  <Card style={{ width: '50%', margin: '5%' }}>
+    <CardHeader>
       {users.map(user =>
-        <ListItem primaryText={ user.name } />
+        <FlatButton label={user.name} />
       )}
-    </List>
-    <List>
+    </CardHeader>
+    <CardMedia
+      overlay={<CardTitle title={name} />}
+    >
+      <img src="http://lorempixel.com/600/337/nature/" />
+    </CardMedia>
+    <CardTitle title="Destinations" />
+    <CardText>
       {destinations.map(destination =>
-        <ListItem primaryText={ destination.location } />
+        <FlatButton label={`${destination.location}:
+          ${destination.startDate}-${destination.endDate}`}
+        />
       )}
-    </List>
-  </div>
+    </CardText>
+  </Card>
 );
 
 TripItem.propTypes = {
