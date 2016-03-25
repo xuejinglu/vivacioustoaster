ad11d03a38ba37ee09e63ebfc7c80a6ca095c855
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-25 11:56:13 -0700

    Tentatively remove tagsController

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const Strategy = require('passport-facebook').Strategy;
 const destinationsController = require('../destinations/destinationsController');
 const eventsController = require('../events/eventsController');
-const tagsController = require('../tags/tagsController');
+// const tagsController = require('../tags/tagsController');
 const tripsController = require('../trips/tripsController');
 const placeSearchController = require('../placeSearch/placeSearchController');
 const usersController = require('../users/usersController');
@@ -34,8 +34,8 @@ module.exports = function (app, express) {
   app.get('/api/destinations/:destId/events', eventsController.getAll);
 
   // Tags
-  app.post('/api/events/:eventId/tags', tagsController.createAll);
-  app.get('/api/events/:eventId/tags', tagsController.getAll);
+  // app.post('/api/events/:eventId/tags', tagsController.createAll);
+  // app.get('/api/events/:eventId/tags', tagsController.getAll);
 
   // Votes
   app.get('/api/destinations/:destId/events/:eventId/votes'/* , controller function here */);
