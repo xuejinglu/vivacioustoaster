c53210316ad41ad76ac2572068e385b0119a43e8
Leran <leranf#gmail.com>
2016-03-25 10:40:50 -0700

    adds onClick method to myTrips to start dispatching actions

--- client/app/header.js
+++ client/app/header.js
@@ -4,15 +4,23 @@ import Immutable from 'immutable';
 import { AppBar, Tabs, Tab } from 'material-ui';
 import { Link } from 'react-router';
 import { logout } from '../auth/authActions';
+import { fetchTrips } from '../trip/tripActions';
 
+const mapStateToProps = state => ({
+  user: state.query.get('events'),
+});
 
 const mapDispatchToProps = dispatch => ({
   onLogout: id => dispatch(logout()),
+  onClickTrips: () => dispatch(fetchTrips()),
 });
 
-let Header = ({ onLogout }) => (
+let Header = ({ onLogout, onClickTrips }) => (
   <AppBar title="voyAger" showMenuIconButton={false}>
-    <Link to="/trip"><Tab label="My Trips" /></Link>
+    <Link to="/trip"><Tab label="My Trips" onClick={ () =>
+      onClickTrips() }
+    />
+    </Link>
     <Link to="/home"><Tab label="Home" /></Link>
     <Tab label="Logout" onClick={() => onLogout()} />
   </AppBar>
@@ -20,6 +28,7 @@ let Header = ({ onLogout }) => (
 
 Header.propTypes = {
   onLogout: React.PropTypes.func.isRequired,
+  onClickTrips: React.PropTypes.func.isRequired,
 };
 
 Header = connect(null, mapDispatchToProps)(Header);
