947f512a617836d742d6751e1868dd27618d63c8
Leran <leranf#gmail.com>
2016-03-26 16:40:25 -0700

    sets minDate for startDate to today and minDate for endDate to startDate

--- client/home/destSearch.js
+++ client/home/destSearch.js
@@ -7,26 +7,30 @@ import DatePicker
 from '../../node_modules/material-ui/lib/date-picker/date-picker';
 import Geosuggest from 'react-geosuggest';
 
+const mapStateToProps = state => ({
+  startDate: state.home.getIn(['destination', 'startDate']),
+});
+
 const mapDispatchToProps = dispatch => ({
   onChangeDestination: destination => dispatch(changeDestination(destination)),
   onChangeStartDate: newStartDate => dispatch(changeStartDate(newStartDate)),
   onChangeEndDate: newEndDate => dispatch(changeEndDate(newEndDate)),
 });
 
-let DestSearch = ({ onChangeDestination, onChangeStartDate, onChangeEndDate }) => (
+let DestSearch = ({ startDate, onChangeDestination, onChangeStartDate, onChangeEndDate }) => (
   <div>
-    <Geosuggest placeholder="ex: London" onBlur={(event) =>
+    <Geosuggest placeholder="ex: London" onBlur={event =>
       event ? onChangeDestination(event.value) : null
     }
-      onSuggestSelect = {(suggest) => {
+      onSuggestSelect = {suggest => {
         onChangeDestination(suggest.label);
       }}
     />
-    <DatePicker hintText="Departure Date" onChange={(event, newDate) => {
+    <DatePicker hintText="Departure Date" minDate={new Date()} onChange={(event, newDate) => {
       onChangeStartDate(newDate);
     }}
     />
-    <DatePicker hintText="End Date" onChange={(event, newDate) => {
+    <DatePicker hintText="End Date" minDate={startDate} onChange={(event, newDate) => {
       onChangeEndDate(newDate);
     }}
     />
@@ -38,7 +42,8 @@ DestSearch.propTypes = {
   onChangeDestination: React.PropTypes.func.isRequired,
   onChangeStartDate: React.PropTypes.func.isRequired,
   onChangeEndDate: React.PropTypes.func.isRequired,
+  startDate: React.PropTypes.object.isRequired,
 };
 
-DestSearch = connect(null, mapDispatchToProps)(DestSearch);
+DestSearch = connect(mapStateToProps, mapDispatchToProps)(DestSearch);
 export default DestSearch;
