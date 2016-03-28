fdb5474390fee9d27fee989f14be6dd5918cd123
Boya Jiang <jiang.boya@yahoo.com>
2016-03-28 10:26:33 -0700

    create add button for additional friends, non functional

--- client/trip/tripItem.js
+++ client/trip/tripItem.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import Card from 'material-ui/lib/card/card';
+import { setTripAndGetDestinations } from '../tripPlan/tripPlanActions';
 import CardActions from 'material-ui/lib/card/card-actions';
 import CardHeader from 'material-ui/lib/card/card-header';
 import CardMedia from 'material-ui/lib/card/card-media';
 import CardTitle from 'material-ui/lib/card/card-title';
 import FlatButton from 'material-ui/lib/flat-button';
 import CardText from 'material-ui/lib/card/card-text';
+import ListItem from 'material-ui/lib/lists/list-item';
+import Avatar from 'material-ui/lib/avatar';
 import Moment from 'moment';
 
 const TripItem = ({ key, name, tripType, destinations, users }) => (
   <Card style={{ width: '50%', margin: '5%' }}>
-    <CardHeader>
+    <ListItem primaryText="Add more friends" className="addFriendsButton"
+      leftAvatar={<Avatar src={'../../assets/add.png'}
+        style={{ width: '1em', height: '1em', marginTop: '7px' }}
+      />}
+      style={{ marginBottom:'-10px' }}
+      onClick={ () => startSetTripAndGetDestinations(key)}
+    />
+    <CardHeader
+      style={ { marginTop: '-15px', marginBottom: '-10px' }}
+    >
       {users.map(user =>
         <FlatButton label={user.name} />
       )}
     </CardHeader>
     <CardMedia
+      onClick={}
       overlay={<CardTitle title={name} />}
     >
       <img src="http://lorempixel.com/600/337/nature/" />
@@ -31,6 +44,14 @@ const TripItem = ({ key, name, tripType, destinations, users }) => (
     </CardText>
   </Card>
 );
+
+FriendList.propTypes = {
+  friends: React.PropTypes.object.isRequired,
+  onFriendClick: React.PropTypes.func.isRequired,
+};
+
+FriendList = connect(mapStateToProps, mapDispatchToProps)(FriendList);
+
 
 TripItem.propTypes = {
   key: React.PropTypes.number,
