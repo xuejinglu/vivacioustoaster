d879c51f161c72220c7600074b948f48417ccd8c
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-28 15:21:17 -0700

    (bugfix) Set NODE_ENV properly

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const pg = require('pg');
 const Sequelize = require('sequelize');
 let db;
 
-if (process.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   db = new Sequelize(process.env.DATABASE_URL);
 } else {
   db = new Sequelize('voyager', null, null, {
