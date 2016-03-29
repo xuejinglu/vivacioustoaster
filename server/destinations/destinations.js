ef86328e1136001524e9177f615ab164fa32f140
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-29 16:10:11 -0700

    Add photoUrl property to destination schema

--- server/destinations/destinations.js
+++ server/destinations/destinations.js
@@ -1,11 +1,13 @@
 const db = require('../config/db');
 const Sequelize = require('sequelize');
 const Trip = require('../trips/trips');
+const getDestinationPhotos = require('./flickrSearch');
 
 const Destination = db.define('destinations', {
   startDate: Sequelize.DATE,
   endDate: Sequelize.DATE,
   location: Sequelize.STRING,
+  photoUrl: Sequelize.STRING,
 });
 
 // creates tripId column in destinations table
@@ -17,10 +19,14 @@ Trip.hasMany(Destination, { constraints: false });
 Destination.sync();
 Trip.sync();
 
-Destination.createDestinations = (destinations) =>
-  // returning true to gain access to the destination ID from the db.
-  Destination.bulkCreate(destinations, { returning: true }).catch(err => err);
-
+Destination.createDestinations = (destinations) => {
+  getDestinationPhotos(destinations)
+    .then(destinationsWithPhotos =>
+      // returning true to gain access to the destination ID from the db.
+      Destination.bulkCreate(destinationsWithPhotos, { returning: true }).catch(err => err)
+    )
+    .catch(err => err);
+};
 
 Destination.getDestinations = tripId =>
   Destination.findAll({ where: { tripId } }).catch(err => err);
