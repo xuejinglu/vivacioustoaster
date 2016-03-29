09fc9238d32723aa881fbd79734c54aca6927fc5
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-29 16:09:48 -0700

    Add flickr photo search

---
+++ server/destinations/flickrSearch.js
@@ -0,0 +1,46 @@
+const rp = require('request-promise');
+const Promise = require('bluebird');
+const FLICKR_PHOTOSEARCH_URL = 'http://flickr.com/services/rest';
+
+const FARM_ID = '{FARM_ID}';
+const SERVER_ID = '{SERVER_ID}';
+const PHOTO_ID = '{PHOTO_ID}';
+const PHOTO_SECRET = '{PHOTO_SECRET}';
+const FLICKR_TEMPLATE_URL = `https://farm${FARM_ID}.staticflickr.com/${SERVER_ID}/${PHOTO_ID}_${PHOTO_SECRET}.jpg`;
+
+const constructPhotoUrl = photo =>
+  FLICKR_TEMPLATE_URL.replace(FARM_ID, photo.farm)
+                     .replace(SERVER_ID, photo.server)
+                     .replace(PHOTO_ID, photo.id)
+                     .replace(PHOTO_SECRET, photo.secret);
+
+const getDestinationPhotos = destinations => {
+  const optionsArray = destinations.map(destination => ({
+    uri: FLICKR_PHOTOSEARCH_URL,
+    qs: {
+      method: 'flickr.photos.search',
+      key: process.env.FLICKR_PHOTOS_KEY,
+      format: 'json',
+      text: destination.location,
+      sort: 'interestingness-desc',
+      is_getty: true,
+      media: 'photos',
+    },
+    headers: {
+      'User-Agent': 'Request-Promise',
+    },
+    json: true, // Automatically parses the JSON string in the response
+  }));
+
+  return Promise.all(optionsArray.map((options, index) =>
+    rp(options)
+      .then(data => {
+        const photo = data.results.photos.photo[0];
+        const destination = destinations[index];
+
+        destination.photoUrl = constructPhotoUrl(photo);
+        return destination;
+      })
+      .catch(err => err)
+  ));
+};
