5ee3a49fffbbfde8633068ac9035ea15c44e07ef
Leran <leranf#gmail.com>
2016-03-30 10:49:38 -0700

    add votes to eventlist

--- client/tripPlan/event/eventList.js
+++ client/tripPlan/event/eventList.js
@@ -6,26 +6,29 @@ import CardTitle from 'material-ui/lib/card/card-title';
 
 const mapStateToProps = state => ({
   events: state.tripPlan.event.get('events'),
+  votes: state.tripPlan.vote.get('votes'),
   destinations: state.tripPlan.dest.get('destinations'),
 });
 
-let EventList = ({ destinations, destIdx, events }) => (
+let EventList = ({ destinations, destIdx, events, votes }) => (
   <div>
     <CardTitle
       title={`Places To See In ${destinations[destIdx].location}`}
       style={{ textAlign: 'center' }}
     />
     <List>
-      {events.get(destIdx).map(event =>
-        <EventItem key={ event.id } {...event} />
-      )}
+      {events.get(destIdx).map(event => {
+        const eventVotes = votes.filter(vote => vote.eventId === event.id);
+        return <EventItem key={ event.id } {...event} votes={eventVotes} />;
+      })}
     </List>
   </div>
 );
 
 EventList.propTypes = {
   destinations: React.PropTypes.object.isRequired,
-  events: React.PropTypes.object.isRequired,
+  events: React.PropTypes.array.isRequired,
+  votes: React.PropTypes.array.isRequired,
   destIdx: React.PropTypes.number.isRequired,
 };
 
