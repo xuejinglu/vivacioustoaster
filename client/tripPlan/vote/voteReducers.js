70acdd8ea89606d5ae8bafe6c074b58ba6f7ef3e
Leran <leranf#gmail.com>
2016-03-30 19:20:33 -0700

    handles the vote actions in the reducer

--- client/tripPlan/vote/voteReducers.js
+++ client/tripPlan/vote/voteReducers.js
@@ -1,6 +1,8 @@
 import Immutable from 'immutable';
+import { REQUEST_VOTES, RECEIVE_VOTES_FOR_EVENTS, FETCH_VOTES_FAILURE } from './voteActions';
 
 const initialState = Immutable.Map({
+  isFetching: false,
   votes: Immutable.List(),
 });
 
@@ -13,7 +15,10 @@ const vote = (state, action) => {
 
 const votes = (state = initialState, action) => {
   switch (action.type) {
-    case 'RECEIVE_VOTES_IN_EVENTS':
+    case REQUEST_VOTES:
+      return state.set('isFetching', true);
+    case RECEIVE_VOTES_FOR_EVENTS:
+      state.set('isFetching', false);
       return state.set('votes', action.payload.votes);
     default:
       return state;
