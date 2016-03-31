c8c98e8e0963712a92d02001a3fdec0063020b85
Leran <leranf#gmail.com>
2016-03-30 19:19:54 -0700

    fetches votes for events and updates state

--- client/tripPlan/vote/voteActions.js
+++ client/tripPlan/vote/voteActions.js
@@ -1,28 +1,44 @@
+import _ from 'lodash';
 import fetch from 'isomorphic-fetch';
 import cookie from 'react-cookie';
 import Promise from 'bluebird';
 
+export const REQUEST_VOTES = 'REQUEST_VOTES';
+export const RECEIVE_VOTES_FOR_EVENTS = 'RECEIVE_VOTES_FOR_EVENTS';
+export const FETCH_VOTES_FAILURE = 'FETCH_VOTES_FAILURE';
+
+const requestVotes = () => ({
+  type: REQUEST_VOTES,
+});
+
 const receiveVotesForEvents = votes => ({
-  type: 'RECEIVE_VOTES_FOR_EVENTS',
+  type: RECEIVE_VOTES_FOR_EVENTS,
   payload: {
     votes,
   },
 });
 
-export const fetchVotes = events => {
-  const token = cookie.load('token');
-  return dispatch =>
-    Promise.all(events.map(event =>
-      fetch(`/api/event/${event.id}/votes`, {
+const fetchVotesError = message => ({
+  type: FETCH_VOTES_FAILURE,
+  payload: {
+    // TODO
+  },
+});
+
+export const fetchVotes = events =>
+  dispatch => {
+    dispatch(requestVotes());
+    return Promise.all(events.map(event =>
+      fetch(`/api/events/${event.id}/votes`, {
         method: 'GET',
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
-          token,
         },
       }).then(res => res.json())
     )).then(votes => {
-      dispatch(receiveVotesForEvents(votes));
+      const allVotes = _.flattenDeep(votes);
+      dispatch(receiveVotesForEvents(allVotes));
     })
     .catch(err => console.error(err)); // add proper error handling
-};
+  };
