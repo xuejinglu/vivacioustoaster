8b6b43bf21d6b7115a35632c0d094e9e5137a9ef
Akshay Buddiga <abuddiga@stanford.edu>
2016-04-01 00:30:43 -0700

    Add spacing for button

--- client/tripPlan/dest/destItem.js
+++ client/tripPlan/dest/destItem.js
@@ -29,9 +29,11 @@ let DestItem = ({ destIdx, key, location, startDate, endDate, onChooseDest, onCl
     </CardMedia>
     <Link to="tag">
       <RaisedButton
+        secondary
         label="Add Events"
         onMouseDown={ () => onChooseDest(destIdx) }
         onMouseUp={ () => onClearDest() }
+        style = { { margin: '16px' } }
       />
     </Link>
     <EventList destIdx={destIdx} />
@@ -43,7 +45,6 @@ DestItem.propTypes = {
   location: React.PropTypes.string.isRequired,
   startDate: React.PropTypes.any.isRequired,
   endDate: React.PropTypes.any.isRequired,
-  events: React.PropTypes.any.isRequired,
   onChooseDest: React.PropTypes.func.isRequired,
   onClearDest: React.PropTypes.func.isRequired,
   destIdx: React.PropTypes.any,
