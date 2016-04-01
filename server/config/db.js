db7fff7a16f23b6968b739e043b51121b8c00363
Akshay Buddiga <abuddiga@stanford.edu>
2016-04-01 09:12:21 -0700

    Use regular Sequelize connection in production

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,12 @@ const Sequelize = require('sequelize');
 let db;
 
 if (process.env.NODE_ENV === 'production') {
-  db = new Sequelize(process.env.DATABASE_URL);
+  db = new Sequelize('voyager', process.env.DB_USER, process.env.DB_PASSWORD, {
+    dialect: 'postgres',
+    define: {
+      underscored: false,
+    },
+  });
 } else {
   db = new Sequelize('voyager', null, null, {
     dialect: 'postgres',
