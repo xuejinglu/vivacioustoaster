898ceb2bd2aea6bf592513319b1e603d44e951b2
Akshay Buddiga <abuddiga@stanford.edu>
2016-04-01 00:00:59 -0700

    Remove apostrophe

--- client/tripPlan/tripOverview/destOverview.js
+++ client/tripPlan/tripOverview/destOverview.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import List from 'material-ui/lib/lists/list';
-import ListItem from 'material-ui/lib/lists/list-item';
 import Card from 'material-ui/lib/card/card';
-import CardHeader from 'material-ui/lib/card/card-header';
-import CardMedia from 'material-ui/lib/card/card-media';
-import CardTitle from 'material-ui/lib/card/card-title';
 import CardText from 'material-ui/lib/card/card-text';
-import Avatar from 'material-ui/lib/avatar';
 
 const mapStateToProps = state => ({
   destinations: state.tripPlan.dest.get('destinations'),
