c7661692a7039a7db545129793a38315d1d311cb
Akshay Buddiga <abuddiga@stanford.edu>
2016-04-01 09:01:29 -0700

    Make tripOverview a stateless component

--- client/tripPlan/tripOverview/tripOverview.js
+++ client/tripPlan/tripOverview/tripOverview.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import TravelFriendList from './travelFriendList';
 import DestOverview from './destOverview';
 import Card from 'material-ui/lib/card/card';
 import CardTitle from 'material-ui/lib/card/card-title';
 
-const mapStateToProps = state => ({
-  destinations: state.tripPlan.dest.get('destinations'),
-});
-
-let TripOverview = ({ destinations }) => (
+const TripOverview = () => (
   <div>
     <Card style={ { width: '60%', margin: '2.5% 20%' } }>
       <CardTitle title="Let's Take Off!" style={{ textAlign: 'center' }} />
@@ -18,11 +13,5 @@ let TripOverview = ({ destinations }) => (
     </Card>
   </div>
 );
-
-TripOverview.propTypes = {
-  destinations: React.PropTypes.object.isRequired,
-};
-
-TripOverview = connect(mapStateToProps)(TripOverview);
 
 export default TripOverview;
