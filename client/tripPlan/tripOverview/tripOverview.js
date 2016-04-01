e2c089ef241cba44d8b78cd97f5bea6c6f6963b2
Akshay Buddiga <abuddiga@stanford.edu>
2016-03-31 23:55:11 -0700

    Complete trip Overview card

--- client/tripPlan/tripOverview/tripOverview.js
+++ client/tripPlan/tripOverview/tripOverview.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import TravelFriendItem from './travelFriendItem';
-import TravelFriendSecondaryList from './travelFriendSecondaryList';
+import TravelFriendList from './travelFriendList';
 import DestOverview from './destOverview';
-import List from 'material-ui/lib/lists/list';
-import ListItem from 'material-ui/lib/lists/list-item';
 import Card from 'material-ui/lib/card/card';
-import CardHeader from 'material-ui/lib/card/card-header';
-import CardMedia from 'material-ui/lib/card/card-media';
 import CardTitle from 'material-ui/lib/card/card-title';
-import CardText from 'material-ui/lib/card/card-text';
-import Avatar from 'material-ui/lib/avatar';
 
 const mapStateToProps = state => ({
   destinations: state.tripPlan.dest.get('destinations'),
@@ -21,7 +14,7 @@ let TripOverview = ({ destinations }) => (
     <Card style={ { width: '60%', margin: '5% 20%' } }>
       <CardTitle title="Let's Take Off!" style={{ textAlign: 'center' }} />
       <DestOverview />
-      <TravelFriendSecondaryList />
+      <TravelFriendList />
     </Card>
   </div>
 );
